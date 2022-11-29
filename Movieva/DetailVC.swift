436e949bd95e1bf3ac6f6a624108b07c72348c13
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-11-29 23:40:05 +0300

    add detail vc

---
+++ Movieva/DetailVC.swift
@@ -0,0 +1,21 @@
+//
+//  DetailVC.swift
+//  Movieva
+//
+//  Created by Mehmet Kerim ÖZEK on 29.11.2022.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+    
+
+}
