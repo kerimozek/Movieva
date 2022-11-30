8f6765a314c4f7cfb0feccbd1282482582ed663b
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-11-30 14:42:04 +0300

    add search

---
+++ Movieva/Search/View/Cell/SearchCell.swift
@@ -0,0 +1,19 @@
+//
+//  SearchCell.swift
+//  Movieva
+//
+//  Created by Mehmet Kerim ÖZEK on 30.11.2022.
+//
+
+import UIKit
+
+class SearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+   
+    
+}
