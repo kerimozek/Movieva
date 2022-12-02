9d3043bb9e492e207b79a0636c7a6e3c44d0de9f
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-03 00:46:23 +0300

    add watch list cell

--- Movieva/WatchList/View/Cell/WatchListCell.swift
+++ Movieva/WatchList/View/Cell/WatchListCell.swift
@@ -9,10 +9,39 @@ import UIKit
 
 class WatchListCell: UITableViewCell {
 
+    
+    @IBOutlet weak var watchListCellView: UIView!
+    @IBOutlet weak var watchListImageView: UIImageView!
+    @IBOutlet weak var movieTitle: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var movieMinutes: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var imdbLabel: UILabel!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        watchListImageView.backgroundColor = UIColor(red: 0.85, green: 0.20, blue: 0.31, alpha: 1.00)
+        watchListImageView.layer.cornerRadius = 10
+        watchListImageView.layer.shadowColor = UIColor.darkGray.cgColor
+        watchListImageView.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
+        watchListImageView.layer.shadowRadius = 4.0
+        watchListImageView.layer.shadowOpacity = 0.4
+        
+        watchListImageView.layer.cornerRadius = 10
+        watchListImageView.clipsToBounds = true
     }
 
+    func configureCell(item: ResultMovie) {
+        let image2 = "https://pbs.twimg.com/media/E_FxqkSVgAQZKPM.jpg"
+        self.movieTitle.text = item.title
+        if item.posterPath != nil {
+            let image = NetworkHelper.shared.baseImageUrl + item.posterPath!
+            self.watchListImageView.kf.setImage(with: URL(string: image))
+        } else {
+            self.watchListImageView.kf.setImage(with: URL(string: image2))
+        }
+    //    self.genreLabel.text = item.originalLanguage
+    //    self.movieMinutes.text = item.releaseDate
+        self.detailLabel.text = item.overview
+    }
 
 }
--- Movieva/WatchList/ViewModel/WatchListVM.swift
+++ Movieva/WatchList/ViewModel/WatchListVM.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol WatchListViewModelDelegate: AnyObject {
+    func didGetMovies(isDone: Bool)
+}
+
+
+class WatchListVM {
+    
+    static let shared = WatchListVM()
+    weak var delegate: WatchListViewModelDelegate?
+    private init () { }
+    
+    var watchListMovie: [ResultMovie] = []
+
+    func getMovies(complete: @escaping((String?)->())) {
+        PopularManager.shared.getPopular { [self] items, errorMessage in
+            
+            if let items = items {
+                self.watchListMovie = items.results!
+                self.delegate?.didGetMovies(isDone: true)
+            }
+            complete(errorMessage)
+            self.delegate?.didGetMovies(isDone: false)
+        }
+    }
+    
+}
