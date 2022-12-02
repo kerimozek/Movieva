da4404e133eae0f001a030b90fbe4ae480ad91e7
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-02 14:18:20 +0300

    add container views detail screen

---
+++ Movieva/Detail/Containers/ContainerViewAbout.swift
@@ -0,0 +1,23 @@
+//
+//  ContainerViewAbout.swift
+//  Movieva
+//
+//  Created by Mehmet Kerim ÖZEK on 2.12.2022.
+//
+
+import UIKit
+
+class ContainerViewAbout: UIViewController {
+
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textView.isEditable = false
+    }
+    
+
+    
+
+}
--- Movieva/Detail/DetailVC.swift
+++ Movieva/Detail/DetailVC.swift
@@ -9,13 +9,50 @@ import UIKit
 
 class DetailVC: UIViewController {
 
+    @IBOutlet weak var imageViewTop: UIImageView!
+    @IBOutlet weak var containerViewAbout: UIView!
+    @IBOutlet weak var containerViewReviews: UIView!
+    @IBOutlet weak var containerViewCast: UIView!
+    @IBOutlet weak var littleView: UIView!
+    @IBOutlet weak var imageViewBottom: UIImageView!
+    
+    @IBOutlet weak var selectedSegment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupUI()
        
     }
     
+    private func setupUI() {
+        imageViewBottom.layer.cornerRadius = 6
+        selectedSegment.selectedSegmentIndex = 0
+        containerViewReviews.alpha = 0
+        containerViewCast.alpha = 0
+    }
+    
 
+    @IBAction func changeContainer(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            UIView.animate(withDuration: 0.5, animations: {
+                        self.containerViewAbout.alpha = 1
+                        self.containerViewReviews.alpha = 0
+                        self.containerViewCast.alpha = 0
+                    })
+        } else  if sender.selectedSegmentIndex == 1 {
+                    UIView.animate(withDuration: 0.5, animations: {
+                        self.containerViewAbout.alpha = 0
+                        self.containerViewReviews.alpha = 1
+                        self.containerViewCast.alpha = 0
+                    })
+                } else  {
+                    UIView.animate(withDuration: 0.5, animations: {
+                        self.containerViewAbout.alpha = 0
+                        self.containerViewReviews.alpha = 0
+                        self.containerViewCast.alpha = 1
+                    })
+                }
+    }
     
 
 }
