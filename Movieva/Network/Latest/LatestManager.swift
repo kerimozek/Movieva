df8d88905e968f2018be2ba024f2b77b1faa2619
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-05 22:09:55 +0300

    add latest manager

---
+++ Movieva/Network/Latest/LatestManager.swift
@@ -0,0 +1,31 @@
+//
+//  LatestManager.swift
+//  Movieva
+//
+//  Created by Mehmet Kerim ÖZEK on 5.12.2022.
+//
+
+import Foundation
+
+class LatestManager {
+    
+    static let shared = LatestManager()
+    
+    // MARK: - LATEST https://api.themoviedb.org/3/movie/now_playing?api_key=fd24fe5ba58021d3f54a2a7c04297951&language=en-US&page=1 -
+    
+    func getLatest(page: Int, complete: @escaping((Latest?, String?)->())) {
+        
+        let url = APIURLs.latestMovies(page: page)
+        
+        NetworkManager.shared.request(type: Latest.self,
+                                      url: url,
+                                      method: .get) { response in
+            switch response {
+            case .success(let items):
+                complete(items, nil)
+            case .failure(let error):
+                complete(nil, error.rawValue)
+            }
+        }
+    }
+}
