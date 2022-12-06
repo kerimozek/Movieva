e0b4247ca5d0f8e98117bbde0938e1a40111fef2
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-06 13:02:12 +0300

    refactor main viewmodel

--- Movieva/Main/ViewModel/MainVM.swift
+++ Movieva/Main/ViewModel/MainVM.swift
@@ -7,10 +7,8 @@
 
 import Foundation
 
-@objc protocol MainVMDelegate: AnyObject {
-    @objc optional func didGetTopRated(isDone: Bool)
-    @objc optional func didGetPopular(isDone: Bool)
-    @objc optional func didGetLatest(isDone: Bool)
+protocol MainVMDelegate: AnyObject {
+    func didGetTopRated(isDone: Bool)
 }
 
 class MainVM {
@@ -20,8 +18,6 @@ class MainVM {
     private init () { }
     
     var topRated: [ResultMovie] = []
-    var popular: [ResultMovie] = []
-    var latest: [ResultMovie] = []
     private var page: Int = 1
 
     func getTopRated(complete: @escaping((String?)->())) {
@@ -30,36 +26,10 @@ class MainVM {
             if let items = items {
                 self.topRated.append(contentsOf: items.results!)
                 self.page += 1
-                self.delegate?.didGetTopRated!(isDone: true)
+                self.delegate?.didGetTopRated(isDone: true)
             }
             complete(errorMessage)
-            self.delegate?.didGetTopRated!(isDone: false)
-        }
-    }
-    
-    func getPopular(complete: @escaping((String?)->())) {
-        PopularManager.shared.getPopular(page: page) { items, errorMessage in
-            
-            if let items = items {
-                self.popular.append(contentsOf: items.results!)
-                self.page += 1
-                self.delegate?.didGetPopular!(isDone: true)
-            }
-            complete(errorMessage)
-            self.delegate?.didGetPopular!(isDone: false)
-        }
-    }
-    
-    func getLatest(complete: @escaping((String?)->())) {
-        LatestManager.shared.getLatest(page: page) { items, errorMessage in
-            
-            if let items = items {
-                self.latest.append(contentsOf: items.results!)
-                self.page += 1
-                self.delegate?.didGetLatest!(isDone: true)
-            }
-            complete(errorMessage)
-            self.delegate?.didGetPopular!(isDone: false)
+            self.delegate?.didGetTopRated(isDone: false)
         }
     }
 }
