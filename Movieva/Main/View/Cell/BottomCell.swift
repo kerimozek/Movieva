849355da7365f28f59d0be7f50dff9a3996f0c2a
Mehmet Kerim ÖZEK <kerimantik198@gmail.com>
2022-12-22 15:50:26 +0300

    refactor bottomcell

--- Movieva/Main/View/Cell/BottomCell.swift
+++ Movieva/Main/View/Cell/BottomCell.swift
@@ -15,14 +15,11 @@ class BottomCell: UICollectionViewCell {
     @IBOutlet weak var bottomCellImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-    //    bottomCellView.backgroundColor = UIColor(red: 0.08, green: 0.06, blue: 0.22, alpha: 1.00)
         bottomCellView.layer.cornerRadius = 10
         bottomCellView.layer.shadowColor = UIColor.darkGray.cgColor
         bottomCellView.layer.shadowOffset = CGSize(width: 3.0, height: 3.0)
         bottomCellView.layer.shadowRadius = 4.0
         bottomCellView.layer.shadowOpacity = 0.4
-        
         bottomCellImageView.layer.cornerRadius = 10
         bottomCellImageView.clipsToBounds = true
         
